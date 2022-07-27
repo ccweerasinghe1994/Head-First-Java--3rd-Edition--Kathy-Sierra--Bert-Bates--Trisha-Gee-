10d9ef5deebce724b613f711f4c883144b658861
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2022-07-27 07:22:17 +0530

    🆕: first comit

---
+++ chapter-01/Song.java
@@ -0,0 +1,24 @@
+public class Song{
+  
+  
+  
+  public static void main(String[] args){
+    int bottleCount = 10;
+    String word = "bottles";
+
+    while(bottleCount>0){
+      if(bottleCount==1){
+        word = "bottle";
+      }
+      System.out.println(bottleCount + " green " + word + " hanging on the wall,");
+      System.out.println(bottleCount + " green " + word + " hanging on the wall,");
+      System.out.println("And if one green bottle should accidentally fall,");
+      bottleCount--;
+      if(bottleCount>1){
+        System.out.println("There will be " + bottleCount + " green " + word + " hanging on the wall,");
+      }else{
+      System.out.println("There will be no green bottles hanging on the wall,");
+      }
+    }
+  }
+}
